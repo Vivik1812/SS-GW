a4d058f778b714e0b0f4293b9985759defb5fe5b
Vivik1812 <an.verde@duocuc.cl>
2026-05-10 23:39:53 -0400

    Arreglo

--- Downloads/ruteando-main/ruteando-main/src/components/atoms/Text.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function Text({ children, variant = 'p', className }){
-    const Tag = variant;
-    return <Tag className={className}>{children}</Tag>;
-}
-
-export default Text;
